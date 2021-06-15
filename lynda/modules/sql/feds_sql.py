25439480c1eed3d7a696a226e48ab66d655ac7e8
kari-36 <83143204+kari-36@users.noreply.github.com>
2021-06-16 02:52:31 +0500

    Update feds_sql.py

--- lynda/modules/sql/feds_sql.py
+++ lynda/modules/sql/feds_sql.py
@@ -1,7 +1,7 @@
 import threading
 
-from tg_bot import dispatcher
-from tg_bot.modules.sql import BASE, SESSION
+from lynda import dispatcher
+from lynda.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
 from telegram.error import BadRequest, Unauthorized
 
