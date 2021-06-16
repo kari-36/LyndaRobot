8712804283b8c52749f2f21ab9495f4cbfefb95a
kari-36 <83143204+kari-36@users.noreply.github.com>
2021-06-16 16:36:36 +0500

    Update zipper.py

--- lynda/modules/zipper.py
+++ lynda/modules/zipper.py
@@ -3,7 +3,7 @@ import time
 import os
 from lynda.events import register
 from lynda import TEMP_DOWNLOAD_DIRECTORY
-from lynda import telethn as client
+from lynda.services.client import  client
 from telethon import types
 from telethon.tl import functions
 
