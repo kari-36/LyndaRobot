598d8513309dee3b0c61b6200722a5970c65d687
kari-36 <83143204+kari-36@users.noreply.github.com>
2021-06-16 14:45:09 +0500

    Update extra.py

--- lynda/modules/extra.py
+++ lynda/modules/extra.py
@@ -468,11 +468,12 @@ returns what you're scrobbling on last.fm.
 finds an app in playstore for you
 
 ──「 *Zipper:* 」──
- -> `/zip` reply to a telegram file to compress it in .zip format
- -> `/unzip reply to a telegram file to decompress it from the .zip format
+ -> `/zip`
+ reply to a telegram file to compress it in .zip format
+ -> `/unzip
+ reply to a telegram file to decompress it from the .zip format
 """
 
-"""
 APP_HANDLER = DisableAbleCommandHandler("app", app)
 UD_HANDLER = DisableAbleCommandHandler("ud", ud)
 COVID_HANDLER = DisableAbleCommandHandler(["covid", "corona"], covid)
