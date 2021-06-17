4556c20377dd5cb491f60bd0433e4705bce92f2b
kari-36 <83143204+kari-36@users.noreply.github.com>
2021-06-18 01:26:09 +0500

    Update __init__.py

--- lynda/__init__.py
+++ lynda/__init__.py
@@ -81,6 +81,7 @@ if ENV:
     DEL_CMDS = bool(os.environ.get('DEL_CMDS', False))
     STRICT_GBAN = bool(os.environ.get('STRICT_GBAN', False))
     WORKERS = int(os.environ.get('WORKERS', 8))
+    PATTERNS = os.environ.get("PATTERNS", ".;!,")
     BAN_STICKER = os.environ.get('BAN_STICKER', 'CAADAgADOwADPPEcAXkko5EB3YGYAg')
     ALLOW_EXCL = os.environ.get('ALLOW_EXCL', False)
     CASH_API_KEY = os.environ.get('CASH_API_KEY', None)
