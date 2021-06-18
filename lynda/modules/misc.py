1f255fbdfbf8394794f068ec69c2e2b777cb996d
kari-36 <83143204+kari-36@users.noreply.github.com>
2021-06-18 13:18:02 +0500

    Update misc.py

--- lynda/modules/misc.py
+++ lynda/modules/misc.py
@@ -223,7 +223,12 @@ def stats(update: Update, _):
     result = re.sub(r'(\d+)', r'<code>\1</code>', stats)
     r = requests.get("https://api.waa.ai/v2/links/Lynda").json()
     update.effective_message.reply_text(result, parse_mode=ParseMode.HTML)
-
+    kb = [
+          [
+           InlineKeyboardButton('Channel', url='t.me/up'),
+           InlineKeyboardButton('Support', url='t.me/YorktownEagleUnion')
+          ]
+    ]
 
 __help__ = """
 -> `/id`
